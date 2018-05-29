0b9456d358a2ddba6ab83cb5fcdd2f426b6e230e
Eduardo Hernandez <egh1795@gmail.com>
2018-05-29 01:44:54 -0500

    I made sum and substraction functions

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "mylib.hpp"
 
 using namespace std;
 
+int sum(int a, int b);
+
 int  main(){
 	cout << "Hello world!" << endl;
+	cout << "5 + 10 = " << sum(5,10) << endl;
 	return 0;
+}
+
+int sum (int a, int b){
+	return a+b;
 }
---
+++ mylib.hpp
@@ -0,0 +1,3 @@
+int substraction(int a, int b){
+	return a-b;
+}
